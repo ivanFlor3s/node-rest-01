59242fdec139a2bdf8a6dc5c0ba49ba99196ae03
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-08-19 22:26:17 -0300

    Validacion de rol contra db Roles

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,7 @@ const { usuariosGet,
         usuariosDelete, 
         usuariosPut } = require("../controllers/users.controller");
 const { validarCampos } = require("../middlewares/validar-campos");
+const Role = require('../model/role')
 
 const router = Router();
 
@@ -15,7 +16,13 @@ router.post("/",[
     check('correo', "El correo no es valido").isEmail(),
     check('nombre', "El nombre es obligatorio").not().isEmpty(),
     check('password', "La contrasena es obligatoria y mas de 6 letras").isLength({min: 6}),
-    check('rol', "No es un rol permitido").isIn(['ADMIN_ROLE','USER_ROLE']),
+    //check('rol', "No es un rol permitido").isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom( async (rol = "") => {
+        const existeRol = await Role.findOne({rol});
+        if( !existeRol){
+            throw new Error(`El rol ${rol} no esta registrado en la DB`)
+        }
+    }),
     validarCampos
 ]  , usuariosPost);
 
