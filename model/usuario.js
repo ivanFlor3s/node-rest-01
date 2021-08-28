a663a53a976d48228482a8d34c771bd312738519
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-08-27 22:37:30 -0300

    cambio uid en lugar de _id en modelo

--- model/usuario.js
+++ model/usuario.js
@@ -34,7 +34,8 @@ const UsuarioSchema = Schema({
 
 UsuarioSchema.methods.toJSON = function () {
   //Saco la version, la password y todo lo demas lo dejo en usuario
-  const { __v, password, ...usuario } = this.toObject();
+  const { __v, password, _id , ...usuario } = this.toObject();
+  usuario.uid = _id
   return usuario;
 };
 
