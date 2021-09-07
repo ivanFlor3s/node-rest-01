cd937b9012b4eb3948afc036c57e182f99dffd6f
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2021-09-07 00:03:32 -0300

    buscarUsuario

--- controllers/buscar.controller.js
+++ controllers/buscar.controller.js
@@ -1,13 +1,56 @@
 
 const { response, request } = require("express");
+const { isValidObjectId } = require("mongoose");
+const { Usuario } = require("../model");
+
+const coleccionPermitidas = [
+    'categoria',
+    'usuarios',
+    'productos',
+    'roles'
+]
+
+const buscarUsuario = async (termino= '', res = response) =>{
+
+    const esMongoId = isValidObjectId(termino)
+
+    if(esMongoId){
+        const usuario = await Usuario.findById(termino)
+        res.json({
+            results: usuario ? [usuario] : []
+        })
+    }
+}
 
 const buscar = (req = request, res = response) => {
 
     const {coleccion, termino} = req.params
 
-    res.json({
-        coleccion, termino
-    })
+    if(!coleccionPermitidas.includes(coleccion)){
+        return res.status(400).json({
+            msg: `Las correcciones permitidas son: ${coleccionPermitidas}`
+        })
+    }
+
+    switch (coleccion){
+        case 'categoria':
+
+        break;
+
+        case 'usuarios':
+            buscarUsuario(termino, res)
+        break;
+
+        case 'productos':
+
+        break;
+
+        default: 
+            res.status(400).json({
+                msg: 'Coleccion busqueda Not implemented'
+            })
+
+    }
 }
 
 module.exports = {
